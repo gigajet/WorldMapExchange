d6cc9316c4b68c98bc5b5e58eb9cbaaa3dbf32ee
xununa <gigajet08092000@gmail>
2020-09-17 00:13:35 +0000

    modeChooseActivity : chọn mode trước khi vô

---
+++ app/src/main/java/com/example/worldmapexchange/ModeChooseActivity.java
@@ -0,0 +1,50 @@
+package com.example.worldmapexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ModeChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_choose);
+    }
+
+    public void onClickModeChooseButtons(View view) {
+        switch (view.getId()) {
+            case R.id.btnModeLength:
+                Resources.getInstance().chosenMode=0;
+                break;
+            case R.id.btnModeArea:
+                Resources.getInstance().chosenMode=1;
+                break;
+            case R.id.btnModeMass:
+                Resources.getInstance().chosenMode=2;
+                break;
+            case R.id.btnModeTemperature:
+                Resources.getInstance().chosenMode=3;
+                break;
+            case R.id.btnModeAngle:
+                Resources.getInstance().chosenMode=4;
+                break;
+            case R.id.btnModeEnergy:
+                Resources.getInstance().chosenMode=5;
+                break;
+            case R.id.btnModeSpeed:
+                Resources.getInstance().chosenMode=6;
+                break;
+            case R.id.btnModeTime:
+                Resources.getInstance().chosenMode=7;
+                break;
+            case R.id.btnModeBase:
+                Resources.getInstance().chosenMode=8;
+                break;
+            case R.id.btnModeCurrency:
+                Resources.getInstance().chosenMode=9;
+                break;
+        }
+    }
+}
