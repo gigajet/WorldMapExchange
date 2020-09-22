9574c0d546c8cffc319ed291631c62e0629bbb5f
jojuo123 <vtduy3100@gmail.com>
2020-09-22 13:07:27 +0700

    cut hextring views.py

--- server/myapp2/views.py
+++ server/myapp2/views.py
@@ -31,18 +31,34 @@ def image_upload_view(request):
         #form = ImageForm(request.POST, request.FILES)
         myfile = bytes(request.body)
 
+        temp = myfile.hex()
+
+        #print('hex: ',temp)
+        strbegin = temp.find('89504e470d0a1a0a')
+        strend = temp.find('49454e44ae426082')
+
+        hexstring = temp[strbegin:strend+16]
+
+        myfile2 = bytearray.fromhex(hexstring)
+        
+        #f=open('hexstring.txt','w')
+        #f.write(hexstring)
+        #f.close()
+
         f=open('test_cc.bmp','wb')
-        f.write(myfile)
+        f.write(myfile2)
         f.close()
         f=open('test_cc.bmp','rb')
-
+        
         fs = FileSystemStorage()
         #filename = fs.save(myfile.name,myfile)
         filename = fs.save('test_cc',f)
         path = './media/' + filename
         result = hand_reg.regconize(path)
+        f.close()
         print('Result: ',result)
         print(len(result))
+
         return JsonResponse({'success':result})
     else:
         #form = ImageForm()
